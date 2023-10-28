1f290cf245a43deee4bfe992e14704009dd70c22
Zack Ward <zackward.student@gmail.com>
2023-10-28 10:53:45 -0700

    WIP@ Tile

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -17,12 +17,15 @@ const Tile: FC<TileProps> = ({
   link,
 }) => {
   return (
-    <div className="flex flex-col align-left-top border-2 border-solid border-slate-800 m-11 p-5 rounded-md shadow-md bg-indigo-200 bg-opacity-10 max-w-xs max-h-40">
-      <h2 className="text-2xl font-semibold">{name}</h2>
-      <h3 className="text-xl font-semibold">prep time: {prepTime}</h3>
-      <h3 className="text-xl font-semibold">cook time: {cookTime}</h3>
+    <div className="grid align-left-top border-2 border-solid border-slate-800 m-11 p-5 rounded-md shadow-md bg-indigo-200 bg-opacity-10 max-w-xs max-h-40">
+      <h2 className="text-md font-semibold">{name}</h2>
+      <h3 className="text-xs font-semibold m-1">
+        prep time: {prepTime} cook time: {cookTime}
+      </h3>
       <p>{description}</p>
-      <Link href={link}>View Recipe</Link>
+      <Link href={link} className="text-blue-500">
+        View Recipe
+      </Link>
     </div>
   );
 };
